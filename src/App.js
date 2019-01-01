75796a9a8f758f551c8325514a4124373a04a0b1
Josef Renner <josef.renner@gmail.com>
2019-01-02 00:06:04 +0100

    first human commit

--- src/App.js
+++ src/App.js
@@ -2,24 +2,64 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
+let defaultTextColor = '#fff';
+let defaultStyle = {
+  color: defaultTextColor,
+
+}
+
+class Aggregate extends Component {
   render() {
     return (
+      <section className="aggregate" style={{...defaultStyle, display: 'inline-block'}}>
+        <h2>Number Text</h2>
+      </section>
+    );
+  }
+}
+
+class Filter extends Component {
+  render() {
+    return (
+      <div>
+        <img src="" alt="" />
+        <input type="text" id="filter" />
+        <label for="filter" style={{...defaultStyle, marginLeft: '10px'}}>Filter</label>
+      </div>
+    );
+  }
+}
+
+class Playlist extends Component {
+  render() {
+    return (
+      <div style={{...defaultStyle, width: '25%', display: 'inline-block'}}>
+        <img src="" alt="" />  
+      <h3>Playlist Name</h3>
+      <ul style={defaultStyle}>
+        <li>Song 1</li>
+        <li>Song 2</li>
+        <li>Song 3</li>
+      </ul>
+      </div>
+    );
+  }
+}
+
+class App extends Component {
+  render() {
+    let name = 'Josef';
+    let header_style = { color: 'deeppink', fontSize: '32px', fontWeight: 'bold' }
+    return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>Title</h1>
+        <Aggregate></Aggregate>
+        <Aggregate></Aggregate>
+        <Filter></Filter>
+        <Playlist></Playlist>
+        <Playlist></Playlist>
+        <Playlist></Playlist>
+        <Playlist></Playlist>
       </div>
     );
   }
